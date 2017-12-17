32e6e563494b23da8cebb0a4e50fd0ec0ea78da4
Алексей Дурягин <aleksey.duryagin.n@yandex.ru>
2017-12-17 18:05:57 +0300

    7

--- src/lesson7/task1/Matrix.kt
+++ src/lesson7/task1/Matrix.kt
@@ -48,48 +48,48 @@ fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> =
  * Реализация интерфейса "матрица"
  */
 class MatrixImpl<E>(override val height: Int, override val width: Int, e: E) : Matrix<E> {
-    val listOfLists = MutableList(height) {MutableList(width) {e}}
+    private val list = mutableListOf<E>()
+
+    init {
+        for (i in 0 until height * width) {
+            list.add(e)
+        }
+    }
 
     override fun get(row: Int, column: Int): E =
-            if (row in 0 until height && column in 0 until width &&
-                    listOfLists[row][column].toString().isNotEmpty())
-                listOfLists[row][column]
-            else throw IllegalArgumentException()
-
-    override fun get(cell: Cell): E  = get(cell.row, cell.column)
-
-    override fun set(row: Int, column: Int, value: E)  =
             if (row in 0 until height && column in 0 until width)
-                listOfLists[row][column] = value
+                list[row * width + column]
             else throw IllegalArgumentException()
 
-    override fun set(cell: Cell, value: E) = set(cell.row, cell.column, value)
+    override fun get(cell: Cell): E = list[cell.row * width + cell.column]
 
-    override fun equals(other: Any?) : Boolean {
-        if (!(other is MatrixImpl<*> &&
-                height == other.height &&
-                width == other.width))
-            return false
-        for (row in 0 until height) {
-            for (column in 0 until width)
-                if (listOfLists[row][column] != other[row, column]) return false
-        }
-        return true
+    override fun set(row: Int, column: Int, value: E) {
+        list[row * width + column] = value
     }
+
+    override fun set(cell: Cell, value: E) {
+        list[cell.row * width + cell.column] = value
+    }
+
+    override fun equals(other: Any?): Boolean =
+            other is MatrixImpl<*> &&
+                    height == other.height &&
+                    width == other.width
+
 
     override fun hashCode(): Int {
         var result = height
         result = 31 * result + width
-        result = 31 * result + listOfLists.hashCode()
+        result = 31 * result + list.hashCode()
         return result
     }
 
     override fun toString(): String {
         val stringBuilder = StringBuilder()
         stringBuilder.append("[")
-        for (row in 0 until  height) {
+        for (row in 0 until height) {
             stringBuilder.append("[")
-            for (column in 0 until  width - 1) {
+            for (column in 0 until width - 1) {
                 stringBuilder.append(this[row, column], ", ")
             }
             stringBuilder.append(this[row, width - 1])
